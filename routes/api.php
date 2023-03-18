901fa66da90a3c48f2ff68a633e841e5b2dbf3eb
Gill_zhang <gill.zhang96@gmail.com>
2023-03-18 18:34:08 +0800

    route modify

--- routes/api.php
+++ routes/api.php
@@ -18,26 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', function (Request $request) {
-    try {
-        $params = [
-            'name' => $request->input('name'),
-            'password' => $request->input('password')
-        ];
-        $user = \App\Models\User::where('name', $params['name'])->first();
-        if (blank($user) || !\Illuminate\Support\Facades\Hash::check($params['password'], $user->password)) {
-            return response([
-                'message' => 'username or password 錯誤'
-            ], \Illuminate\Http\Response::HTTP_UNAUTHORIZED);
-        }
-
-        $token = $user->createToken('apiToken')->plainTextToken;
-
-        return response([
-            'user'  => $user,
-            'token' => $token
-        ], \Illuminate\Http\Response::HTTP_CREATED);
-    } catch (Throwable $e) {
-        dd($e);
-    }
-});
+Route::post('/login', [\App\Http\Controllers\UserController::class, 'login']);
